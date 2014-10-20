9d589e54ce30b0d035f0808c56bd254d801b2df3
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2014-10-20 09:37:39 +0200

    Cambiar nombre das variables, introducir longitudDatos por scanner en vez de asignar e facer metodo refactorizando e poñendolle entradaDato();

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,61 +1,76 @@
-
 package refactorizar;
+
+import java.util.Scanner;
 
 /**
  * Lista los numeros primos de dos cifras
- * 
+ *
  */
 public class Refactorizar {
 
-    
     public static void main(String[] args) {
         boolean p = false;
-int numDigitos=0;
-        int ndigitos=0;
-        numDigitos=2;
-        if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int divisionEntera = i;
- 
-            int contador=0;
- 
-            while (divisionEntera != 0)
-        {
-            divisionEntera = divisionEntera / 10;
-            contador++;
+        int longitudDigitos = 0;
+        int contadorDigitos = 0;
+        longitudDigitos = entradaDato();
+
+        if (longitudDigitos <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        ndigitos=contador;
-            
-            
-            if(ndigitos==numDigitos){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite)
-                    {
-                        if (i % i1 == 0) contador1++;
+        for (int i = 1; i <= 99999; i++) {
+            int divisionEntera = i;
+
+            int contador = 0;
+
+            while (divisionEntera != 0) {
+                divisionEntera = divisionEntera / 10;
+                contador++;
+            }
+            contadorDigitos = contador;
+
+            if (contadorDigitos == longitudDigitos) {
+                if (i < 4) {
+                    p = true;
+                } else {
+                    if (i % 2 == 0) {
+                        p = false;
+                    } else {
+                        int contador1 = 0;
+                        int i1 = 1;
+                        int limite = (i - 1) / 2;
+                        if (limite % 2 == 0) {
+                            limite--;
+                        }
+
+                        while (i1 <= limite) {
+                            if (i % i1 == 0) {
+                                contador1++;
+                            }
                             i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
+                            if (contador1 == 2) {
+                                i1 = limite + 1;
+                            }
+                        }
+
+                        if (contador1 == 1) {
+                            p = true;
+                        }
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                }
+
+                if (p == true) {
+                    System.out.println(i);
+                }
             }
         }
     }
-    
+
+    private static int entradaDato() {
+        int longitudDigitos;
+        Scanner dato = new Scanner(System.in);
+        System.out.println("muestrame la longitud digitos");
+        longitudDigitos = dato.nextInt();
+        return longitudDigitos;
+    }
+
 }
